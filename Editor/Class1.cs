3cf5e52e01d11c5d513a07092378c4c837ccd77d
Augustinas-simkus <augustinas.simkus>
2020-06-09 12:51:37 +0300

    Script tests

---
+++ Editor/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class1
+{
+    public void printTest()
+    {
+        Debug.Log("Test123");
+    }
+}
---
+++ Editor/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class2 : MonoBehaviour
+{
+    void Start()
+    {
+        Class1 temp = new Class1;
+        temp.printTest();
+    }
+}
